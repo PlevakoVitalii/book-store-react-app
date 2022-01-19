8553377bd3f0136a04263763873e5acb53faad4b
PlevakoVitalii <plevakovl2102@gmail.com>
2022-01-19 21:57:21 +0200

    refactor: delete unused commits

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,5 +1,9 @@
 import React from 'react';
-import { Route, Routes } from 'react-router-dom';
+import {
+  Route,
+  Routes
+} from 'react-router-dom';
+
 import ShopHeader from '../shop-header';
 import { HomePage, CartPage } from '../pages';
 
@@ -7,20 +11,21 @@ import './app.css';
 
 const App = () => {
   return (
+
     <main role="main" className="container">
-      <ShopHeader numItems={5} total={210}/>
+      <ShopHeader numItems={5} total={210} />
       <Routes>
         <Route
           path="/"
-          element={<HomePage/>}
+          element={<HomePage />}
           exact />
-
         <Route
           path="/cart"
-          element={<CartPage/>}
-          />
+          element={<CartPage />}
+        />
       </Routes>
     </main>
+
   );
 };
 
